1bb1499302003cbd3e43f1720dbcfe3cd5bce797
caiyueliang <393900414@qq.com>
2022-12-15 18:20:27 +0800

    update

---
+++ vision/Taichu-GLIDE/test/test_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# coding=utf-8
+"""
+Author: changwanli
+since: 2022-11-08 06:25:56
+LastTime: 2022-11-10 17:04:53
+LastAuthor: changwanli
+message:
+Copyright (c) 2022 Wuhan Artificial Intelligence Research. All Rights Reserved
+"""
+import requests
+import json
+import os
+
+
+def test(data, host):
+    """
+    [summary]
+
+    Args:
+        line ([type]): [description]
+        mention ([type], optional): [description]. Defaults to None.
+        port (int, optional): [description]. Defaults to 8080.
+
+    Returns:
+        [type]: [description]
+    """
+    my_url = 'http://{host}/predict'.format(host=host)
+    # print(json.dumps(data, ensure_ascii=False, indent=4))
+    my_params = json.dumps(data)
+    header = {'content-type': 'application/json'}
+
+    r = requests.post(url=my_url, data=my_params, headers=header)
+    print("[test] response: {}".format(r))
+    return json.loads(r.text)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default="0.0.0.0:8080", type=str, help='host of service')
+    args = parser.parse_args()
+    data = {}
+    res = test(data, host=args.host)
+    print(json.dumps(res, ensure_ascii=False, indent=4))

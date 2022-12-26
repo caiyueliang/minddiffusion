40193de00d650effb010650106cac8100fd14dca
caiyueliang <393900414@qq.com>
2022-12-26 10:34:14 +0800

    update

--- vision/wukong-huahua/src/serving.py
+++ vision/wukong-huahua/src/serving.py
@@ -121,26 +121,124 @@ def test():
         }
         return response(500, **message)
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--is_chinese", default=True, type=bool, help="chinese or not")
-    parser.add_argument("--denoise_steps", default=1, type=int, help="denoise steps")
-    parser.add_argument("--super_res_step", default=1, type=int, help="super res step")
-    parser.add_argument("--guidance_scale", default=5, type=int, help="guidance scale")
-    parser.add_argument("--pics_generated", default=8, type=int, help="pic num for each prompt")
 
-    parser.add_argument("--tokenizer_model", default="cog-pretrain.model", help="tokenizer model")
-    parser.add_argument("--gen_ckpt", default="glide_gen.ckpt", help="gen ckpt")
-    parser.add_argument("--super_ckpt", default="glide_super_res.ckpt", help="super ckpt")
-    parser.add_argument("--srgan_ckpt", default="srgan.ckpt", help="srgan ckpt")
-
-    parser.add_argument("--prompts_file", default="prompts.txt", help="prompts file")
-    parser.add_argument("--data_path", default="", help="dataset path")
-    parser.add_argument("--output_path", default="", help="output path")
-    parser.add_argument("--ckpt_path", default="pretraind_models/", type=str, help="ckpt init path")
-    parser.add_argument("--model_config_path", default="./configs/model_config.json", help="model_config")
-
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        nargs="?",
+        default="狗 绘画 写实风格",
+        help="the prompt to render"
+    )
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        nargs="?",
+        help="dir to write results to",
+        default="outputs/txt2img-samples"
+    )
+    parser.add_argument(
+        "--skip_grid",
+        action='store_true',
+        help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
+    )
+    parser.add_argument(
+        "--skip_save",
+        action='store_true',
+        help="do not save individual samples. For speed measurements.",
+    )
+    parser.add_argument(
+        "--ddim_steps",
+        type=int,
+        default=50,
+        help="number of ddim sampling steps",
+    )
+    parser.add_argument(
+        "--plms",
+        action='store_true',
+        help="use plms sampling",
+    )
+    parser.add_argument(
+        "--fixed_code",
+        action='store_true',
+        help="if enabled, uses the same starting code across samples ",
+    )
+    parser.add_argument(
+        "--ddim_eta",
+        type=float,
+        default=0.0,
+        help="ddim eta (eta=0.0 corresponds to deterministic sampling",
+    )
+    parser.add_argument(
+        "--n_iter",
+        type=int,
+        default=2,
+        help="sample this often",
+    )
+    parser.add_argument(
+        "--H",
+        type=int,
+        default=512,
+        help="image height, in pixel space",
+    )
+    parser.add_argument(
+        "--W",
+        type=int,
+        default=512,
+        help="image width, in pixel space",
+    )
+    parser.add_argument(
+        "--n_samples",
+        type=int,
+        default=8,
+        help="how many samples to produce for each given prompt. A.k.a. batch size",
+    )
+    parser.add_argument(
+        "--n_rows",
+        type=int,
+        default=0,
+        help="rows in the grid (default: n_samples)",
+    )
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=7.5,
+        help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
+    )
+    parser.add_argument(
+        "--from-file",
+        type=str,
+        help="if specified, load prompts from this file",
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/v1-inference-chinese.yaml",
+        help="path to config which constructs model",
+    )
+    parser.add_argument(
+        "--ckpt",
+        type=str,
+        default="models/wukong-huahua-ms.ckpt",
+        help="path to checkpoint of model",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="the seed (for reproducible sampling)",
+    )
+    parser.add_argument(
+        "--precision",
+        type=str,
+        help="evaluate at this precision",
+        choices=["full", "autocast"],
+        default="autocast"
+    )
     args = parser.parse_args()
+
     logging.warning(args)
 
     WuKong(args=args)

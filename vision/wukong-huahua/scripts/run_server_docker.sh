325ca4e2848f89006ef9a0fec3da588e9529d2ac
caiyueliang <393900414@qq.com>
2022-12-27 14:37:50 +0800

    update

--- vision/wukong-huahua/scripts/run_server_docker.sh
+++ vision/wukong-huahua/scripts/run_server_docker.sh
@@ -17,7 +17,7 @@
 export STORAGE_MEDIA="OBS"
 
 # filebeat日志采集器
-mkdir - filebeat
+mkdir filebeat
 tar -xvf filebeat.tar.gz -C filebeat
 sed -i "s#log_path#${log_path}#g" filebeat/filebeat.yml
 sed -i "s/app_name/${app_name}/g" filebeat/filebeat.yml
